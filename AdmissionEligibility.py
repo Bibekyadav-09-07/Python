e23fd796d2db7ed1f76e9abf2ef473bb6a248f0c
Bibekyadav-09-07 <bibek.yadav@patancollege.edu.np>
2026-09-20 11:30:15 +0545

    Delete AdmissionEligibility.py

--- AdmissionEligibility.py
+++
@@ -1,7 +0,0 @@
-age = int(input("Enter your age: "))
-qualification = input("Have you completed +2? (yes/no): ")
-
-if age >= 18 and qualification.lower() == "yes":
-    print("You are eligible for admission in bachelor.")
-else:
-    print("You are not eligible for admission bachelor.")
